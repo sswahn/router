f6bed9d6da8a5714dc0b602bfe2638482fb68d4d
sswahn <36422765+sswahn@users.noreply.github.com>
2023-11-01 13:58:10 -0400

    Update router.js

--- src/core/router.js
+++ src/core/router.js
@@ -1,75 +1,41 @@
-import { useState, useContext, useEffect, Suspense, lazy } from 'react'
-import { Context } from './Provider'
-import { api, url } from './config'
-import { getRequest } from './utilities/Server'
-import Loader from './components/loading/Loader'
-//const PostList = lazy(() => import('./components/list/PostList'))
-//const PostListItem = lazy(() => import('./components/list/PostListItem'))
-//const Profile = lazy(() => import('./features/Profile/Profile'))
-const PrivacyPolicyPage = lazy(() => import('./pages/PrivacyPolicyPage/PrivacyPolicyPage'))
-const CookieUsePage = lazy(() => import('./pages/CookieUsePage/CookieUsePage'))
-const TermsAndConditionsPage = lazy(() => import('./pages/TermsAndConditionsPage/TermsAndConditionsPage'))
-const NotFoundComponent = () => <div></div>//<div>404 - Not Found</div>;
+let route = window.location.pathname || '/'
 
-const routeConfig = [
-  //{ path: url.home, component: PostList },
-  { path: url.privacy, component: PrivacyPolicyPage },
-  { path: url.cookie, component: CookieUsePage },
-  { path: url.terms, component: TermsAndConditionsPage },
-  //{ path: '/:country/:city/:id', component: PostListItem },
-  //{ path: '/profile/:username', component: Profile }
-]
-
-const RouteCache = new Map()
-
-export default function Router() {
-  const [route, setRoute] = useState(window.location.pathname || '/')
-  const [context, dispatch] = useContext(Context)
-  
-
+const Router = ({ routes, notFoundComponent, beforeRouteChange, afterRouteChange }) => {
   const matchRoute = path => {
-    if (RouteCache.has(path)) {
-      return RouteCache.get(path)
-    }
-    const matchedRoute = routeConfig.find(({ path: routePath }) => {
+    const matchedRoute = routes.find(({ path: routePath }) => {
       const pathSegments = path.split('/').filter(Boolean)
       const routeSegments = routePath.split('/').filter(Boolean)
       return (
         pathSegments.length === routeSegments.length &&
-        routeSegments.every((segment, index) => segment.startsWith(':') || segment === pathSegments[index])
+        routeSegments.every(
+          (segment, index) => segment.startsWith(':') || segment === pathSegments[index]
+        )
       )
     })
-    const component = matchedRoute ? matchedRoute.component : null
-    RouteCache.set(path, component)
-    return component
+    return matchedRoute || null
   }
 
   const renderComponent = () => {
-    const MatchedComponent = matchRoute(route)
-    return MatchedComponent ? <MatchedComponent /> : <NotFoundComponent />
+    const MatchedRoute = matchRoute(route)
+    if (MatchedRoute) {
+      if (beforeRouteChange) {
+        beforeRouteChange(route)
+      }
+      return <MatchedRoute.component />
+    }
+    return notFoundComponent ? <notFoundComponent /> : <NotFoundComponent />
   }
-  
+
   const handleRouteChange = () => {
-    dispatch({ type: 'router', payload: window.location.pathname })
+    if (afterRouteChange) {
+      afterRouteChange(window.location.pathname)
+    }
+    route = window.location.pathname
   }
-  
-  useEffect(() => {
-    // handles browser navigation: back/forward buttons
-    window.addEventListener('popstate', handleRouteChange)
-    return () => {
-      window.removeEventListener('popstate', handleRouteChange)
-    }
-  }, [])
-  
-  useEffect(() => {
-    if (context.router === window.location.pathname) {
-      setRoute(context.router)
-    }
-  }, [context.router])
-  
-  return (
-    <Suspense fallback={<div className="center"><Loader /></div>}>
-      {renderComponent()}
-    </Suspense>
-  )
+
+  window.addEventListener('popstate', handleRouteChange)
+
+  return renderComponent()
 }
+
+export default Router
