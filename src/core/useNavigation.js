8f9827a34e47f50f3f5767232c1b1f63ce1710d5
sswahn <36422765+sswahn@users.noreply.github.com>
2023-11-04 10:50:54 -0400

    Update and rename useNavigate.js to useNavigation.js

--- src/core/useNavigation.js
+++ src/core/useNavigation.js
@@ -1,13 +1,12 @@
 import { useContext } from 'react'
 import { Context } from './Provider'
 
-export function useNavigate() {
+export function useNavigation() {
   const { dispatch } = useContext(Context)
-
+  
   const navigateTo = (path) => {
     history.pushState(null, '', path)
     dispatch({ type: 'router', payload: path })
   }
-
-  return navigateTo;
+  return navigateTo
 }
