57531fa61e3e19e92d333c88023e3550bad4527c
sswahn <36422765+sswahn@users.noreply.github.com>
2023-11-04 13:35:06 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import Router from './core/Router'
-import { useNavigation } from './core/useNavigation'
+import Router from './core/Router.js'
+import { useNavigation } from './core/useNavigation.js'
 
 export {
   Router,
