3b6a88149a300369301ab8add509fc0fe6b7b64e
sswahn <36422765+sswahn@users.noreply.github.com>
2023-11-06 09:36:24 -0500

    Update Router.js

--- src/core/Router.js
+++ src/core/Router.js
@@ -1,11 +1,27 @@
+import { useEffect, Suspense } from 'react'
 import Provider from './Provider.js'
 import Handler from './Handler.js'
 
 export default function Router({ lazyFallback, children }) {
+
+  useEffect(() => { // handles browser navigation: back/forward buttons
+    window.addEventListener('popstate', handleRouteChange)
+    return () => {
+      window.removeEventListener('popstate', handleRouteChange)
+    }
+  }, [])
+  
+  useEffect(() => { // handles programatic navigation: navigateTo
+    if (context.router === window.location.pathname) {
+      setRoute(context.router)
+    }
+  }, [context.router])
+  
   return (
     <Provider>
-      <Handler lazyFallback={lazyFallback} />
-      {children}
+      <Suspense fallback={lazyFallback && <lazyFallback />}>
+        {children}
+      </Suspense>
     </Provider>
   )
 }
