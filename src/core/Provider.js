84496db8802540f75f5b0098ff7fdd02664f8297
sswahn <36422765+sswahn@users.noreply.github.com>
2023-11-06 15:32:11 -0500

    Create Provider.js

---
+++ src/core/Provider.js
@@ -0,0 +1,22 @@
+
+export const Context = createContext([])
+
+export default function Provider({ children }) {
+  const data = {
+    router: window.location.pathname || '/'
+  }
+  function reducer(state, action) {
+    switch(action.type) {
+      case 'router':
+        return { ...state, router: action.payload }
+      default:
+        return state
+    }
+  }
+  const [context, dispatch] = useReducer(reducer, data)
+  return (
+    <Context.Provider value={{context, dispatch}}>
+      {children}
+    </Context.Provider>
+  )
+}
