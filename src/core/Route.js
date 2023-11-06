dbe04f538b35ed00cd7e6aff5734eb47bca95940
sswahn <36422765+sswahn@users.noreply.github.com>
2023-11-06 11:41:46 -0500

    Update Route.js

--- src/core/Route.js
+++ src/core/Route.js
@@ -1,5 +1,5 @@
 const Route = ({ path, component }) => {
   const isMatchingRoute = path ? window.location.pathname === path : true
-  return isMatchingRoute && <Component /> 
+  return isMatchingRoute && <component /> 
 }
 export default Route
