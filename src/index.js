0c80739828a4dfc311c7ed5f482e2e0a94937314
sswahn <36422765+sswahn@users.noreply.github.com>
2023-11-06 12:29:12 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import Router from './core/Router.js'
 import Route from './core/Route.js'
-import { RouterContext } from './core/Router.js'
+import { navigateTo } from './core/navigateTo.js'
 
 export {
   Router,
   Route,
-  RouterContext
+  navigateTo
 }
