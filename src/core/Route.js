2e5576418f3debb1681f7b8d1fff3c90dcc100ea
sswahn <36422765+sswahn@users.noreply.github.com>
2023-11-06 15:08:41 -0500

    Update Route.js

--- src/core/Route.js
+++ src/core/Route.js
@@ -1,4 +1,8 @@
 const Route = ({ path, component: Component }) => {
+  
+  console.log('Route path: ', path)
+  console.log('Route pathname: ', window.location.pathname)
+  
   const isMatchingRoute = path ? window.location.pathname === path : true
   return isMatchingRoute && <Component /> 
 }
