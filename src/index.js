c7e034ae490190d6b9c66b0feb6679f2a575cd88
sswahn <36422765+sswahn@users.noreply.github.com>
2023-11-06 11:35:31 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Router from './core/Router.js'
 import Route from './core/Route.js'
-import { RouterContext } from './core/Provider.js'
+import { RouterContext } from './core/Router.js'
 
 export {
   Router,
