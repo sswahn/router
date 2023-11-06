553941ae33cbc029a93c5edaa16cf74c0dd0eedf
sswahn <36422765+sswahn@users.noreply.github.com>
2023-11-06 09:18:23 -0500

    Update Router.js

--- src/core/Router.js
+++ src/core/Router.js
@@ -1,7 +1,7 @@
 import Provider from './Provider.js'
 import Handler from './Handler.js'
 
-export default function Router({ routes, notFound, lazyFallback, children }) {
+export default function Router({ lazyFallback, children }) {
   return (
     <Provider>
       <Handler routes={routes} notFound={notFound} lazyFallback={lazyFallback} />
