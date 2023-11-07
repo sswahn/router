44acc301a8f6b422d3763f4b6476f00fc88ce920
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-07 13:04:58 -0500

    Update Route.js

--- src/core/Route.js
+++ src/core/Route.js
@@ -1,4 +1,11 @@
 const Route = ({ component: Component }) => {
+
+  console.log('Component: ', Component)
+
+  console.log('typeof Component: ', typeof Component)
+
+  console.log('typeof Component === "function": ', typeof Component === 'function')
+  
   return typeof Component === 'function' ? Component() : Component
 }
 export default Route
