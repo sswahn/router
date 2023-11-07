990d15eb21e48ab0d816fe4a9a8261ec3b84951a
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-07 11:41:07 -0500

    Update Route.js

--- src/core/Route.js
+++ src/core/Route.js
@@ -1,5 +1,5 @@
 const Route = ({ component: Component }) => {
-  if (typeof Component === 'function' && (typeof Component !== 'object' && Component.$$typeof !== Symbol.for('react.element')) {
+  if (typeof Component === 'function' && (typeof Component !== 'object' && Component.$$typeof !== Symbol.for('react.element'))) {
     const Result = Component()
     return <Result />
   } else {
